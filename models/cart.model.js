f7ccb1017ea9f0c467fb8f1b8e17fbd269fb2d9d
Goga <g.bagauri@yahoo.com>
2025-04-26 22:57:52 +0400

    recovered cart model

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,19 +1,36 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  items: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-      },
-      quantity: { type: Number, default: 1 },
-    },
-  ],
+const CartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [1, "Quantity cannot be less than 1"],
+  },
 });
 
-const Cart = mongoose.model("Cart", cartSchema);
+const CartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [CartItemSchema],
+    totalAmount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cart = mongoose.model("Cart", CartSchema);
 
 export default Cart;
