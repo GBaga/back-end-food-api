f4c8fd572a93d8b38def89c7b34ec9245525b68a
Goga <g.bagauri@yahoo.com>
2025-04-29 00:04:17 +0400

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const register = async (req, res) => {
+export const register = async (req, res) => {
   try {
     const { name, email, password, mobile, secondMobile } = req.body;
 
@@ -18,7 +18,7 @@ const register = async (req, res) => {
       email,
       password: hashedPassword,
       mobile,
-      secondMobile, // Optional
+      secondMobile,
     });
 
     res.status(201).json({ message: "User registered successfully" });
@@ -30,39 +30,27 @@ const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    console.log("Received login request:", req.body);
-
     const { email, password } = req.body;
 
-    // Validate input first
     if (!email || !password) {
-      console.log("Missing email or password");
       return res
         .status(400)
         .json({ message: "Email and password are required" });
     }
 
     const user = await User.findOne({ email });
-    if (!user) {
-      console.log("User not found");
-      return res.status(404).json({ message: "User not found" });
-    }
+    if (!user) return res.status(404).json({ message: "User not found" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      console.log("Invalid password");
+    if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
-    }
 
     const token = jwt.sign(
       { _id: user._id, isAdmin: user.isAdmin },
       process.env.JWT_SECRET,
-      {
-        expiresIn: "7d",
-      }
+      { expiresIn: "7d" }
     );
 
-    console.log("Login successful");
     res.status(200).json({ token });
   } catch (error) {
     console.error("Login error:", error);
@@ -72,7 +60,7 @@ export const login = async (req, res) => {
 
 export const getMe = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id).select("-password"); // Use _id consistently
+    const user = await User.findById(req.user._id).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
     res.status(200).json(user);
   } catch (error) {
